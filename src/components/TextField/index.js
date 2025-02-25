a32314b8dd6e2a220e095314c6a442b6b8b3e8ad
Oleksandr <shkutko93@gmail.com>
2025-02-25 21:46:10 +0200

    Add validation to inputs.

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,17 +1,57 @@
 import { FormControl, InputLabel, OutlinedInput, FormHelperText } from "@mui/material";
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import dayjs from 'dayjs';
+import 'dayjs/locale/uk';
 
 export default function TextField({props}) {
+  const inputStyles = {
+    width: 300,
+    maxWidth: '100%',
+  };
+
+  const minDate = dayjs(props.minDate, 'DD.MM.YYYY');
+
   return (
-    <FormControl disabled={props.disabled}>
-      {props.label
-        && <InputLabel htmlFor={props.id}>{props.label}</InputLabel>}
-      <OutlinedInput
-        inputProps={props}
-        aria-describedby={props.helperText && (props.id + "-helper-text")}
-        label={props.label}
-      />
-      {props.helperText 
-        && <FormHelperText id={props.id + "-helper-text"}>{props.helperText}</FormHelperText>}
-    </FormControl>
+    props.type === 'date'
+      ? (
+          <LocalizationProvider  dateAdapter={AdapterDayjs} adapterLocale="uk">
+            <DemoContainer components={['DatePicker']}>
+              <DatePicker
+                minDate={minDate}
+                onChange={props.onChange}
+                slotProps={{
+                  textField: {
+                    helperText: props.helperText,
+                    style: inputStyles,
+                    error: props.error,
+                  },
+                }}
+                aria-describedby={props.helperText && (props.id + "-helper-text")}
+                label={props.label}
+              />
+            </DemoContainer>
+          </LocalizationProvider>
+        )
+      : (
+          <FormControl disabled={props.disabled} error={props.error} style={inputStyles}>
+            {props.label
+              && <InputLabel htmlFor={props.id}>{props.label}</InputLabel>}
+            <OutlinedInput
+              value={props.value}
+              id={props.id}
+              type={props.type}
+              aria-describedby={props.helperText && (props.id + "-helper-text")}
+              label={props.label}
+              onChange={props.onChange}
+              inputProps={{
+                min: props.min >= 0 ? props.min : 'false'
+              }}
+            />
+            {props.helperText 
+              && <FormHelperText id={props.id + "-helper-text"}>{props.helperText}</FormHelperText>}
+          </FormControl>
+        )
   )
 }
--- src/pages/MonthInfo/components/CalculationForm/index.js
+++ src/pages/MonthInfo/components/CalculationForm/index.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Box, Button } from "@mui/material";
 import Grid from '@mui/material/Grid2';
 import TextField from "components/TextField";
+import dayjs from 'dayjs';
 
 export default function CalculationForm({dateOfSelling}) {
   const [dateOfArrival, setDateOfArrival] = useState('');
   const [summInCurrency, setSummInCurrency] = useState('');
+  const [dateOfArrivalError, setDateOfArrivalError] = useState(false);
+  const [dateOfArrivalHelperText, setDateOfArrivalHelperText] = useState('');
 
   const defaultInputsConfigs = [
     {
@@ -13,27 +16,50 @@ export default function CalculationForm({dateOfSelling}) {
         id: 'date-of-selling',
         label: 'Дата продажу',
         disabled: true,
-        required: true,
         value: dateOfSelling,
       },
-      size: { xs: 6 }
+      size: { xs: 12, sm: 6 }
     },
     {
       inputConfig: {
         id: 'date-of-arrival',
         label: 'Дата зарахування',
-        required: true,
-        onChange: (e) => {
-          setDateOfArrival(e.target.value);
-        }
+        type: 'date',
+        minDate: dateOfSelling,
+        helperText: dateOfArrivalHelperText,
+        
+        onChange: (date) => {
+          if (
+            !date
+              || !dayjs(date).isValid()
+              || date.isBefore(dayjs(dateOfSelling, 'DD.MM.YYYY'), "day")
+          ) {
+            setDateOfArrival(null);
+            setDateOfArrivalError(true);
+            !date && setDateOfArrivalHelperText('');
+            
+            if (date) {
+              !dayjs(date).isValid()
+                && setDateOfArrivalHelperText('Некоректна дата зарахування');
+              date.isBefore(dayjs(dateOfSelling, 'DD.MM.YYYY'), "day") 
+                && setDateOfArrivalHelperText('Дата зарахування не може бути меншою за дату продажу');
+            }
+          } else {
+            const formatedDate = date ? date.format('L') : null;
+            setDateOfArrival(formatedDate);
+            setDateOfArrivalError(false);
+            setDateOfArrivalHelperText(formatedDate);
+          }
+        },
       },
-      size: { xs: 6 }
+      size: { xs: 12, sm: 6 }
     },
     {
       inputConfig: {
         id: 'summ-in-currency',
         label: 'Сума в USD',
-        required: true,
+        type: 'number',
+        min: 0,
         onChange: (e) => {
           setSummInCurrency(e.target.value);
         }
@@ -41,9 +67,53 @@ export default function CalculationForm({dateOfSelling}) {
     },
   ];
 
+  const [inputConfigs, setInputConfigs] = useState(defaultInputsConfigs);
+
+  useEffect(() => {
+    setInputConfigs(defaultInputsConfigs);
+  }, [dateOfArrivalHelperText, summInCurrency]);
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(dateOfSelling, dateOfArrival, summInCurrency);
+    if (!dateOfArrival || dateOfArrivalError) {
+      setDateOfArrivalError(true);
+      setInputConfigs(inputConfigs.map((config) => {
+        if (config.inputConfig.id === 'date-of-arrival') {
+          return {
+            ...config,
+            inputConfig: {
+              ...config.inputConfig,
+              error: 'true',
+              helperText:
+                dateOfArrivalError 
+                  ? 'Некоректна дата зарахування'
+                  : 'Це поле не може бути пустим',
+            }
+          }
+        }
+        return config;
+      }));
+      console.log('Invalid date of arrival');
+      return;
+    }
+    if (!summInCurrency) {
+      setInputConfigs(inputConfigs.map((config) => {
+        if (config.inputConfig.id === 'summ-in-currency') {
+          return {
+            ...config,
+            inputConfig: {
+              ...config.inputConfig,
+              error: 'true',
+              helperText: 'Це поле не може бути пустим',
+            }
+          }
+        }
+        return config;
+      }));
+      console.log('Invalid summ in currency');
+      return;
+    }
+    console.log(dateOfSelling + '-' + dateOfArrival, 'Summ: ' + summInCurrency);
   }
 
   return (
@@ -54,7 +124,7 @@ export default function CalculationForm({dateOfSelling}) {
       onSubmit={handleSubmit}
     >
       <Grid container spacing={4} justifyContent='center'>
-        {defaultInputsConfigs.map((config) => (
+        {inputConfigs.map((config) => (
           <Grid
             key={config.inputConfig.id}
             display="flex"
